59e034b3493608821864ddf5d98500598f7bc277
adelamarre <adelamarre>
2010-04-14 00:48:46 +0000

    CONTRIB-1979 - Removed reference to XMLWriter class in the DOM connection class

--- version.php
+++ version.php
@@ -1,15 +1,15 @@
-<?php // $Id: version.php,v 1.1.2.6 2010/04/08 15:18:33 adelamarre Exp $
+<?php // $Id: version.php,v 1.1.2.7 2010/04/14 00:48:46 adelamarre Exp $
 
 /**
  * Code fragment to define the version of adobeconnect
  * This fragment is called by moodle_needs_upgrading() and /admin/index.php
  *
  * @author  Your Name <your@email.address>
- * @version $Id: version.php,v 1.1.2.6 2010/04/08 15:18:33 adelamarre Exp $
+ * @version $Id: version.php,v 1.1.2.7 2010/04/14 00:48:46 adelamarre Exp $
  * @package mod/adobeconnect
  */
 
-$module->version  = 2009121503;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2009121504;  // The current module version (Date: YYYYMMDDXX)
 $module->cron     = 0;           // Period for cron to check this module (secs)
 
 ?>
