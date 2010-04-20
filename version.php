5ef412813899e232a72fd817eddb60c159ff77dd
adelamarre <adelamarre>
2010-04-20 00:02:51 +0000

    CONTRIB-1992 - bumpped version number

--- version.php
+++ version.php
@@ -1,15 +1,15 @@
-<?php // $Id: version.php,v 1.7 2010/04/14 16:28:57 adelamarre Exp $
+<?php // $Id: version.php,v 1.8 2010/04/20 00:02:51 adelamarre Exp $
 
 /**
  * Code fragment to define the version of adobeconnect
  * This fragment is called by moodle_needs_upgrading() and /admin/index.php
  *
- * @author  Your Name <your@email.address>
- * @version $Id: version.php,v 1.7 2010/04/14 16:28:57 adelamarre Exp $
+ * @author  Akinsaya Delamarre adelamarre@remote-learner.net
+ * @version $Id: version.php,v 1.8 2010/04/20 00:02:51 adelamarre Exp $
  * @package mod/adobeconnect
  */
 
-$module->version  = 2009121506;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2009121507;  // The current module version (Date: YYYYMMDDXX)
 $module->cron     = 0;           // Period for cron to check this module (secs)
 
 ?>
