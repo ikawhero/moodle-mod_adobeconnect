3f9065106dd619a36504528466454a89a9ff5b40
adelamarre <adelamarre>
2010-04-26 23:44:29 +0000

    CONTRIB-2030 - bumpped version number, added backup and restore feature.

--- version.php
+++ version.php
@@ -1,15 +1,15 @@
-<?php // $Id: version.php,v 1.1.2.10 2010/04/20 00:01:35 adelamarre Exp $
+<?php // $Id: version.php,v 1.1.2.11 2010/04/26 23:44:29 adelamarre Exp $
 
 /**
  * Code fragment to define the version of adobeconnect
  * This fragment is called by moodle_needs_upgrading() and /admin/index.php
  *
  * @author  Akinsaya Delamarre adelamarre@remote-learner.net
- * @version $Id: version.php,v 1.1.2.10 2010/04/20 00:01:35 adelamarre Exp $
+ * @version $Id: version.php,v 1.1.2.11 2010/04/26 23:44:29 adelamarre Exp $
  * @package mod/adobeconnect
  */
 
-$module->version  = 2009121507;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2009121508;  // The current module version (Date: YYYYMMDDXX)
 $module->cron     = 0;           // Period for cron to check this module (secs)
 
 ?>
